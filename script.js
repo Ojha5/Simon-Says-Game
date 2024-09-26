cc71bd04803123f8683ca0c6b95f7d6090ad623e
om <omjha003@gmail.com>
2024-09-26 13:41:54 +0530

    Added logic of gameover when mismatch the input of user

--- script.js
+++ script.js
@@ -23,19 +23,38 @@ let btnFlash = function(btn , flash){
 }
 
 let levelUp = function(){
+    userSeq = [];
     ++level;
     h2.innerText = `Level ${level}`;
 
     let randInd = Math.floor(Math.random() * 4)
     let randColor = btns[randInd] // Yeh color define kar raha hai ki woh div jiska color yeh hai that we want to flash 
     let randBtn = document.querySelector(`.${randColor}`)
-    
+    gameSeq.push(randColor)
+    console.log(gameSeq)
+
     btnFlash(randBtn , "COMP")
+}
+
+function checkAns(ind){
+    if(gameSeq[ind] == userSeq[ind]){
+        if(gameSeq.length == userSeq.length){
+            setTimeout(levelUp , 1000)
+        }
+    }
+    else{
+        h2.innerText = `Game Over!`;
+    }
 }
 
 function keyPress(){
     if(isGameStarted){
         btnFlash(this , "USER")
+        let userColor = this.getAttribute("id")
+        userSeq.push(userColor)
+        console.log(userSeq)
+
+        checkAns(userSeq.length - 1)
     }
 }
 
